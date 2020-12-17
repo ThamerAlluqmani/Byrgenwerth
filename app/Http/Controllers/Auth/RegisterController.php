9de678f168b67d27bf11afe47fb8e89cc7faed8c
Thamer Alluqmani <69012280+ThamerAlluqmani@users.noreply.github.com>
2020-12-17 04:28:46 +0300

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
+use http\Env\Request;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -69,5 +70,11 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+
+    }
+
+    public function test(Request $request){
+        return dd($request);
     }
 }
