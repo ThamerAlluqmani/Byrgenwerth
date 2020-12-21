efc66457717d8c9c3d2fa638542732100ef18474
Thamer <thamer.allogmani@gmail.com>
2020-12-21 15:45:11 +0300

    change ckeditor to cli

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,7 @@
 {{--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">--}}
 
 
-
-
-{{--    <script src="//cdn.ckeditor.com/4.15.1/full/ckeditor.js"></script>--}}
+    <script src="//cdn.ckeditor.com/4.15.1/full/ckeditor.js"></script>
 </head>
 <body>
 <div id="app">
