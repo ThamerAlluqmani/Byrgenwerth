90ac7ae29d53836a238708d65f6da85f5a74e2cb
Thamer <thamer.allogmani@gmail.com>
2020-12-21 16:57:53 +0300

    test
    1

--- app/Http/Middleware/ChangeLang.php
+++ app/Http/Middleware/ChangeLang.php
@@ -3,20 +3,22 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
+use http\Env\Request;
+use Illuminate\Support\Facades\App;
+
 
 class ChangeLang
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        \App::setLocale($request->language);
+        App::setLocale($request->languge);
 
         return $next($request);
     }
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 
-
-
 Route::group(['prefix' => '{language}'], function () {
 
+    Route::redirect('/', '/ar');
 
-
-    Route::get('/' , 'PageControl@index')->name('index');
+    Route::get('/', 'PageControl@index')->name('index');
     Auth::routes();
     Route::get('dashboard', 'HomeController@index')->name('dashboard');
-    Route::resource('articles' , 'ArticleController');
-    Route::post('comments/{article}' , 'CommentController@store')->name('comments.store');
+    Route::resource('articles', 'ArticleController');
+    Route::post('comments/{article}', 'CommentController@store')->name('comments.store');
     Route::post('ckeditor/image_upload', 'CKEditorController@upload')->name('upload');
-
 
 
 });
