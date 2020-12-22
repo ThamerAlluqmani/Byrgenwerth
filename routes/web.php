781c01beb4cce85fbe9d523d169904755fd33071
Thamer <thamer.allogmani@gmail.com>
2020-12-22 22:32:34 +0300

    1
    1

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,11 @@ Route::get('/' , 'PageControl@index')->name('index');
 Auth::routes();
 Route::get('dashboard', 'HomeController@index')->name('dashboard');
 Route::resource('articles' , 'ArticleController');
+Route::post('ckeditor/image_upload', 'CKEditorController@upload')->name('upload');
 Route::delete('comments/{article}' , 'CommentController@destroy')->name('comments.destroy');
 Route::post('comments/{article}' , 'CommentController@store')->name('comments.store');
 //Auth::routes(['verify' => true]);
-Route::post('ckeditor/image_upload', 'CKEditorController@upload')->name('upload');
+
 
 
 
