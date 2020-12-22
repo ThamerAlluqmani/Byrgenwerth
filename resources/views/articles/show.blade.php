06a550cec1c42f02b1163cc17c8143526b5788de
Thamer <thamer.allogmani@gmail.com>
2020-12-22 18:02:19 +0300

    1
    1

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -40,6 +40,16 @@
         </div>
     </div>
 
+    <div class="card-body">
+        <div class="align-items-center">
+            <a href="{{route('articles.edit', $article)}}" class="btn btn-warning">{{__("Edit")}}</a>
+            <form method="post" action="{{route('articles.destroy', $article)}}" style="display: inline-block">
+                @method('DELETE')
+                @csrf
+                <button onclick="return confirm('{{__("Are you sure you want to delete this article ?")}}')" class="btn btn-danger">{{__("Delete")}}</button>
+            </form>
+        </div>
+
     <div class="text-muted mt-4">
         <h4>
             {{__("Comments")}}
