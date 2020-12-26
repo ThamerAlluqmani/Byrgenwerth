df67c1938b1957483f5a7c804e65a29493e20960
Thamer <thamer.allogmani@gmail.com>
2020-12-26 20:04:30 +0300

    1
    1

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/' , 'PageControl@index')->name('index');
 Route::get('/contact' , 'PageControl@contact')->name('contact');
-Route::get('profile' , 'ProfileControl@index')->name('profile');
 Auth::routes();
 Route::get('dashboard', 'HomeController@index')->name('dashboard');
 Route::resource('articles' , 'ArticleController');
+Route::resource('profile' , 'ProfileControl');
+
 Route::post('ckeditor/upload', 'CKEditorController@upload')->name('ckeditor.image-upload');
 Route::post('comments/{article}' , 'CommentController@store')->name('comments.store');
 Route::delete('comments/{comment}' , 'CommentController@destroy')->name('comments.destroy');
