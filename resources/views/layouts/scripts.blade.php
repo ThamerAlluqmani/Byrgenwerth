b5453b96e3a526fbfcc41708d9882967a30a6a3a
Thamer <thamer.allogmani@gmail.com>
2020-12-31 00:31:43 +0300

    1
    1

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,4 +1,4 @@
-<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/app.js') }}"></script>
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
